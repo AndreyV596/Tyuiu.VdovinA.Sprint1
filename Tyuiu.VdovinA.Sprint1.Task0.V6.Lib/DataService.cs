bfc16dec486c0452336988cce3e6ba64a9fe9af3
AWUD <andrey.work596@gmail.com>
2025-10-02 20:10:27 +0500

    Add project files.

---
+++ Tyuiu.VdovinA.Sprint1.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.VdovinA.Sprint1.Task0.V6.Lib
+{
+    public class DataService
+
+    {
+        public double Calculate()
+        {
+            return 3 * (4 + 1);
+        }
+    }
+}
---
+++ Tyuiu.VdovinA.Sprint1.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VdovinA.Sprint1.Task0.V6.Lib;
+
+
+
+namespace Tyuiu.VdovinA.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
