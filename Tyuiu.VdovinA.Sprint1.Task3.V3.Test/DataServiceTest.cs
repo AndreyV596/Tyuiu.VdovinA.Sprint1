5a5dd5f668b63078f99bd8f2c15aa9c14c4312bb
AWUD <andrey.work596@gmail.com>
2025-10-05 12:33:56 +0500

    i die

---
+++ Tyuiu.VdovinA.Sprint1.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.VdovinA.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
