78f61d9868407e1f35a01fa1f8c3c89734e508c5
AWUD <andrey.work596@gmail.com>
2025-10-13 03:45:26 +0500

    Task5

---
+++ Tyuiu.VdovinA.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VdovinA.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.VdovinA.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int k = 130985;
+            DataService ds = new DataService();
+            int res = ds.Calculate(k);
+
+            int wait = 9;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VdovinA.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VdovinA.Sprint1.Task5.V3.Lib;
+
+//ЗАДАНИЕ
+//Присвоить целой переменной h третью от конца цифру в записи положительного целого числа k
+
+namespace Tyuiu.VdovinA.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите положительное целое число k:");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(k);
+            Console.WriteLine("Третья от конца цифра: " + res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
