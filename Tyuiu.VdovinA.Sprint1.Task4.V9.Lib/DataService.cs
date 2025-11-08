f2ebb981ca5742cc61dfec8bc7c64ac76c15a567
AWUD <andrey.work596@gmail.com>
2025-11-09 00:46:05 +0500

    цуацуац

--- Tyuiu.VdovinA.Sprint1.Task4.V9.Lib/DataService.cs
+++ Tyuiu.VdovinA.Sprint1.Task4.V9.Lib/DataService.cs
@@ -8,7 +8,7 @@ using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.VdovinA.Sprint1.Task4.V9.Lib
 {
-    public class DataService
+    public class DataService : ISprint1Task4V9
     {
         public double Calculate(double x, double y)
         {
